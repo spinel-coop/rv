295912c1ac7a4376698e4a7bb47d74aa4efb8253
André Arko <andre@arko.net>
2025-07-31 15:35:14 -0700

    print the directory

--- crates/rv/src/commands/ruby/install.rs
+++ crates/rv/src/commands/ruby/install.rs
@@ -1,19 +1,16 @@
-use miette::{IntoDiagnostic, Result};
+use miette::Result;
 use owo_colors::OwoColorize;
 
 use crate::config::Config;
 
 pub fn install(config: &Config, version: String) -> Result<()> {
+    let rubies = config.rubies()?;
+    let ruby = rubies.first().unwrap();
+
     println!(
         "Installing Ruby version {} in {}",
         version.cyan(),
-        config
-            .rubies()
-            .unwrap()
-            .first()
-            .map_or("unknown directory".to_string(), |r| r
-                .path()
-                .as_str()
-                .cyan())
+        ruby.path.as_str().cyan()
     );
+    Ok(())
 }
