71a9d705799387c1b2167987ddb7d7166b097d52
André Arko <andre@arko.net>
2025-08-02 22:55:39 -0700

    extract rv-version

--- crates/rv-gem-types/src/lib.rs
+++ crates/rv-gem-types/src/lib.rs
@@ -3,11 +3,10 @@ pub mod name_tuple;
 pub mod platform;
 pub mod requirement;
 pub mod specification;
-pub mod version;
 
 pub use dependency::{Dependency, DependencyError, DependencyType};
 pub use name_tuple::{NameTuple, NameTupleError};
 pub use platform::Platform;
 pub use requirement::{ComparisonOperator, Requirement, VersionConstraint};
+pub use rv_version::{Version, VersionError};
 pub use specification::{Specification, SpecificationError};
-pub use version::{Version, VersionError};
