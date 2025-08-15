7b947e751f04bf2c4b8cc584ece0c8623bde8049
André Arko <andre@arko.net>
2025-08-14 23:06:49 -0700

    start writing up the steps and env vars we still have left

--- crates/rv/src/commands/shell/env.rs
+++ crates/rv/src/commands/shell/env.rs
@@ -14,6 +14,25 @@ type Result<T> = miette::Result<T, Error>;
 
 pub fn env(config: &config::Config) -> Result<()> {
     let ruby = config.rubies().first().cloned();
+    // 1. Remove $RUBY_ROOT/bin from PATH
+    // 2. Remove $GEM_ROOT/bin from PATH
+    // 3. Remove $GEM_HOME/bin from PATH
+    // 4. Remove : from start and end of PATH
+    // 5. Unset all Ruby-related environment variables
+    // 6. Rehash the shell
+    println!("unset RUBY_ROOT RUBY_ENGINE RUBY_VERSION RUBYOPT GEM_ROOT GEM_HOME GEM_PATH");
+    println!("hash -r");
+
+    // 7. Search for a Ruby installation that matches .ruby-version
+    // 8. If found, set the environment variables accordingly
+
+    //  export PATH="${GEM_ROOT:+$GEM_ROOT/bin:}$PATH"
+    // 	export GEM_HOME="$HOME/.gem/$RUBY_ENGINE/$RUBY_VERSION"
+    // 	export GEM_PATH="$GEM_HOME${GEM_ROOT:+:$GEM_ROOT}${GEM_PATH:+:$GEM_PATH}"
+    // 	export PATH="$GEM_HOME/bin:$PATH"
+    //  export GEM_ROOT={ruby.gem_root}
+
+    let ruby = config.rubies()?.first().cloned();
     if let Some(ruby) = ruby {
         print!(
             concat!(
