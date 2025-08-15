4cd3fb194c24929f3eeccd1c53615c1faa5e6cf3
Samuel Giddins <segiddins@segiddins.me>
2025-08-15 12:41:46 -0500

    Add cache command to print cache dir and clean the cache

---
+++ crates/rv/src/commands/cache.rs
@@ -0,0 +1,42 @@
+use std::io;
+
+use bytesize::ByteSize;
+use clap::{Args, Subcommand};
+use owo_colors::OwoColorize;
+use rv_cache::CleanReporter;
+use tracing::info;
+
+use crate::config::Config;
+
+#[derive(Args)]
+pub struct CacheCommandArgs {
+    #[command(subcommand)]
+    pub command: CacheCommand,
+}
+
+#[derive(Subcommand)]
+pub enum CacheCommand {
+    #[command(about = "Clear the cache")]
+    Clean,
+    #[command(about = "Show the cache directory")]
+    Dir,
+}
+
+pub fn cache_dir(config: &Config) -> io::Result<()> {
+    println!("{}", config.cache.root().as_str().cyan());
+    Ok(())
+}
+pub fn cache_clean(config: &Config) -> io::Result<()> {
+    struct Reporter {}
+    impl CleanReporter for Reporter {
+        fn on_clean(&self) {}
+        fn on_complete(&self) {}
+    }
+    let removal = config.cache.clear(Box::new(Reporter {}))?;
+    info!(
+        "Removed {} directories, totalling {}",
+        removal.dirs.default_color().cyan(),
+        ByteSize::b(removal.bytes).display().iec_short().cyan()
+    );
+    Ok(())
+}
