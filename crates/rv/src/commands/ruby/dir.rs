50610d604d5c9096a58bab75865ee4742b749223
RenYan Wei <weirenyan@hotmail.com>
2025-10-17 09:23:04 +0800

    Add `rv ruby dir` command to show ruby installation directory

---
+++ crates/rv/src/commands/ruby/dir.rs
@@ -0,0 +1,16 @@
+use std::io;
+
+use anstream::println;
+use owo_colors::OwoColorize;
+
+use crate::config::{self, Config};
+
+pub fn dir(config: &Config) -> io::Result<()> {
+    let ruby_dir = match config.ruby_dirs.first() {
+        Some(dir) => dir.clone(),
+        None => panic!("No Ruby directories to install into"),
+    };
+
+    println!("{}", ruby_dir.as_str().cyan());
+    Ok(())
+}
