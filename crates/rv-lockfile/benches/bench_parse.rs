ae5f55ebf370db0d9c1297a2c4312cc4ba0ab740
André Arko <andre@arko.net>
2025-10-22 15:34:25 +0900

    rename to bench_parse, fix path, use big lockfile

--- crates/rv-lockfile/benches/bench_parse.rs
+++ crates/rv-lockfile/benches/bench_parse.rs
@@ -4,7 +4,8 @@ use criterion::{Criterion, criterion_group, criterion_main};
 use rv_lockfile::parse;
 
 fn run_bench(c: &mut Criterion, name: &str) {
-    let filepath = format!("crates/rv-lockfile/tests/inputs/{name}");
+    let filepath = format!("./tests/inputs/{name}");
+    println!("benching {filepath}");
     let contents = std::fs::read_to_string(filepath).unwrap();
     c.bench_function(&format!("parse {name}"), |b| {
         b.iter(|| {
@@ -13,13 +14,13 @@ fn run_bench(c: &mut Criterion, name: &str) {
     });
 }
 
-fn parse_example0(c: &mut Criterion) {
-    run_bench(c, "Gemfile.lock.test0");
+fn parse_gitlab(c: &mut Criterion) {
+    run_bench(c, "Gemfile.lock.gitlab");
 }
 
 fn parse_feedyouremail(c: &mut Criterion) {
     run_bench(c, "Gemfile.lock.feedyouremail");
 }
 
-criterion_group!(benches, parse_example0, parse_feedyouremail);
+criterion_group!(benches, parse_gitlab, parse_feedyouremail);
 criterion_main!(benches);
