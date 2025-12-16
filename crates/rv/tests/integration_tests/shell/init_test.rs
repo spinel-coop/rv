310267738464357cad426d4bf4d0ef621f9d0203
David Rodríguez <2887858+deivid-rodriguez@users.noreply.github.com>
2025-12-16 16:49:07 +0100

    Add tests for fish and bash hooks

--- crates/rv/tests/integration_tests/shell/init_test.rs
+++ crates/rv/tests/integration_tests/shell/init_test.rs
@@ -2,9 +2,27 @@ use crate::common::RvTest;
 use insta::assert_snapshot;
 
 #[test]
-fn test_shell_init_succeeds() {
+fn test_zsh_shell_init_succeeds() {
     let test = RvTest::new();
     let output = test.rv(&["shell", "init", "zsh"]);
+    output.assert_success();
+
+    assert_snapshot!(output.normalized_stdout());
+}
+
+#[test]
+fn test_bash_shell_init_succeeds() {
+    let test = RvTest::new();
+    let output = test.rv(&["shell", "init", "bash"]);
+    output.assert_success();
+
+    assert_snapshot!(output.normalized_stdout());
+}
+
+#[test]
+fn test_fish_shell_init_succeeds() {
+    let test = RvTest::new();
+    let output = test.rv(&["shell", "init", "fish"]);
     output.assert_success();
 
     assert_snapshot!(output.normalized_stdout());
