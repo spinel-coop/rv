b62c186a88b54f0c4d0fc64bae3c0d445b0232d0
David Rodríguez <2887858+deivid-rodriguez@users.noreply.github.com>
2025-12-19 06:52:50 +0100

    Add `rv shell <zsh|bash|fish|nu>`
    This command prints setup instructions for each shell. The `init` and
    `completions` subcommands are now hidden.

--- crates/rv/src/commands/shell.rs
+++ crates/rv/src/commands/shell.rs
@@ -2,32 +2,29 @@ pub mod completions;
 pub mod env;
 pub mod init;
 
+use crate::config::Config;
 use clap::{Args, Subcommand};
 use serde::Serialize;
 
 #[derive(Args)]
+#[command(args_conflicts_with_subcommands = true)]
+#[group(required = true, multiple = false)]
 pub struct ShellArgs {
+    #[arg(value_enum)]
+    pub shell: Option<Shell>,
+
     #[command(subcommand)]
-    pub command: ShellCommand,
+    pub command: Option<ShellCommand>,
 }
 
 #[derive(Subcommand)]
 pub enum ShellCommand {
-    #[command(about = "Configure your shell to use rv")]
-    Init {
-        /// The shell to initialize (zsh, bash and fish so far)
-        shell: Shell,
-    },
-    #[command(about = "Configure shell completions to use rv")]
-    Completions {
-        /// The shell to print completions for (zsh, bash and fish so far)
-        shell: Shell,
-    },
     #[command(hide = true)]
-    Env {
-        /// The shell to configure (zsh, bash and fish so far)
-        shell: Shell,
-    },
+    Init { shell: Shell },
+    #[command(hide = true)]
+    Completions { shell: Shell },
+    #[command(hide = true)]
+    Env { shell: Shell },
 }
 
 #[derive(clap::ValueEnum, Clone, Default, Debug, Serialize)]
@@ -37,4 +34,80 @@ pub enum Shell {
     Bash,
     Fish,
     Nu,
+}
+
+impl std::fmt::Display for Shell {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Zsh => write!(f, "zsh"),
+            Self::Bash => write!(f, "bash"),
+            Self::Fish => write!(f, "fish"),
+            Self::Nu => write!(f, "nu"),
+        }
+    }
+}
+
+#[derive(Debug, thiserror::Error, miette::Diagnostic)]
+pub enum Error {
+    #[error(transparent)]
+    IoError(#[from] std::io::Error),
+}
+
+type Result<T> = miette::Result<T, Error>;
+
+pub fn setup(config: &Config, shell: Shell) -> Result<()> {
+    use indoc::{formatdoc, printdoc};
+
+    let name = shell.to_string();
+
+    let header = formatdoc! {"
+        Install rv's shell integration into {name} by running the commands below,
+        or configuring your shell to do the equivalent."
+    };
+
+    let rv = &config.current_exe;
+
+    match shell {
+        Shell::Zsh => {
+            printdoc! {"
+                {header}
+
+                echo 'eval \"$({rv} shell init zsh)\"' >> ~/.zshrc
+                echo 'eval \"$({rv} shell completions zsh)\"' >> ~/.zshrc
+            "};
+
+            Ok(())
+        }
+        Shell::Bash => {
+            printdoc! {"
+                {header}
+
+                echo 'eval \"$({rv} shell init bash)\"' >> ~/.bashrc
+                echo 'eval \"$({rv} shell completions bash)\"' >> ~/.bashrc
+            "};
+
+            Ok(())
+        }
+        Shell::Fish => {
+            printdoc! {"
+                {header}
+
+                echo '{rv} shell init fish | source' >> ~/.config/fish/config.fish
+                echo '{rv} shell completions fish | source' >> ~/.config/fish/config.fish
+            "};
+
+            Ok(())
+        }
+        Shell::Nu => {
+            printdoc! {"
+                {header}
+
+                echo 'mkdir ($nu.data-dir | path join \"vendor/autoload\")
+                {rv} shell init nu | save -f ($nu.data-dir | path join \"vendor/autoload/rv.nu\")
+                {rv} shell completions nu | save --append ($nu.data-dir | path join \"vendor/autoload/rv.nu\")' | save --append $nu.config-path
+            "};
+
+            Ok(())
+        }
+    }
 }
--- docs/SHELL_INTEGRATION.md
+++ docs/SHELL_INTEGRATION.md
@@ -2,34 +2,8 @@
 
 rv integrates with [zsh](#zsh), [bash](#bash), [fish](#fish), and [nushell](#nushell).
 
-Using the configuration below, or something equivalent, will set up `rv` to automatically read any `.ruby-version` or `.tool-versions` file right before every shell command. If necessary, the shell integration will change your PATH, GEM_HOME, and other env vars as needed to ensure that the `ruby` command will run the expected version of ruby.
-
-## zsh
-
-```zsh
-echo 'eval "$(rv shell init zsh)"' >> ~/.zshrc
-echo 'eval "$(rv shell completions zsh)"' >> ~/.zshrc
-```
-
-## bash
-
-```bash
-echo 'eval "$(rv shell init bash)"' >> ~/.bashrc
-echo 'eval "$(rv shell completions bash)"' >> ~/.bashrc
-```
-
-## fish
-
-```fish
-# fish
-echo 'rv shell init fish | source' >> ~/.config/fish/config.fish
-echo 'rv shell completions fish | source' >> ~/.config/fish/config.fish
-```
-
-## nushell
-
-```nushell
-echo 'mkdir ($nu.data-dir | path join "vendor/autoload")
-rv shell init nu | save -f ($nu.data-dir | path join "vendor/autoload/rv.nu")
-rv shell completions nu | save --append ($nu.data-dir | path join "vendor/autoload/rv.nu")' | save --append $nu.config-path
-```
+Run `rv shell <zsh|bash|fish|nu>` to get instructions to set up `rv` to
+automatically read any `.ruby-version` or `.tool-versions` file right before
+every shell command. If necessary, the shell integration will change your
+PATH, GEM_HOME, and other env vars as needed to ensure that the `ruby` command
+will run the expected version of ruby.
