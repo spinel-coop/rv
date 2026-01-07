418a699099f2249240d1ff57bfb2a34d24129e14
Adam Chalmers <adam.s.chalmers@gmail.com>
2026-01-06 21:19:34 -0600

    Start the parser

--- crates/rv-gemfile/src/datatypes.rs
+++ crates/rv-gemfile/src/datatypes.rs
@@ -4,5 +4,43 @@
 #[derive(Debug, Clone, Default, Eq, PartialEq, Ord, PartialOrd, Hash)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub struct Gemfile<'i> {
-    foo: &'i str,
+    #[serde(borrow)]
+    pub items: Vec<Item<'i>>,
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, Ord, PartialOrd, Hash)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+pub enum Item<'i> {
+    Source(&'i str),
+    RubyFile(&'i str),
+    Gem(Gem<'i>),
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, Ord, PartialOrd, Hash)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+pub struct Gem<'i> {
+    #[serde(borrow)]
+    pub name: &'i str,
+    pub constraint: SemverConstraint,
+    #[serde(borrow)]
+    pub version: &'i str,
+}
+/// Constrains the range of possible versions of a gem which could be selected.
+#[derive(Debug, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Copy)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+pub enum SemverConstraint {
+    /// `=`
+    Exact,
+    /// `!=`
+    NotEqual,
+    /// `>`
+    GreaterThan,
+    /// `<`
+    LessThan,
+    /// `>=`
+    GreaterThanOrEqual,
+    /// `<=`
+    LessThanOrEqual,
+    /// `~>`
+    Pessimistic,
 }
--- crates/rv-gemfile/src/parser.rs
+++ crates/rv-gemfile/src/parser.rs
@@ -2,11 +2,10 @@ use crate::{ParseError, ParseErrors, datatypes::*};
 use miette::SourceSpan;
 use winnow::{
     LocatingSlice, ModalResult, Parser,
-    ascii::{line_ending, space0, space1},
-    combinator::{alt, delimited, dispatch, opt, peek, preceded, repeat, separated, terminated},
+    combinator::{delimited, preceded, repeat},
     error::{ContextError, ErrMode},
-    stream::{AsChar, Location, Stream},
-    token::{take_until, take_while},
+    stream::Location,
+    token::take_until,
 };
 
 pub type Input<'a> = LocatingSlice<&'a str>;
@@ -14,17 +13,43 @@ pub type Input<'a> = LocatingSlice<&'a str>;
 type Res<T> = ModalResult<T, ContextError>;
 
 pub fn parse<'i>(file: &'i str) -> Result<Gemfile<'i>, ParseErrors> {
-    let mut input = LocatingSlice::new(file);
+    let mut input = LocatingSlice::new(file.trim());
     let i = &mut input;
-    let mut parsed = Gemfile::default();
-    let mut error: Option<ParseErrors> = None;
-
-    let error = todo!();
-
-    match error {
-        None => Ok(parsed),
-        Some(error) => Err(error),
+    match repeat(1.., parse_item).parse_next(i) {
+        Ok(items) => Ok(Gemfile { items }),
+        Err(e) => {
+            // OK, there was an error. Let's figure out where, to highlight it.
+            let byte_offset = i.location();
+            let char_offset = file[..byte_offset.min(file.len())].chars().count();
+            // Then find the error message.
+            let msg = match &e {
+                ErrMode::Incomplete(_) => "unexpected end of input".to_string(),
+                ErrMode::Backtrack(err) | ErrMode::Cut(err) => err.to_string(),
+            };
+            let parse_err = ParseError {
+                char_offset: SourceSpan::new(char_offset.into(), 1),
+                msg,
+            };
+            Err(ParseErrors {
+                gemfile_contents: file.to_owned(),
+                others: vec![parse_err],
+            })
+        }
     }
+}
+
+fn parse_item<'i>(i: &mut Input<'i>) -> Res<Item<'i>> {
+    parse_source.parse_next(i)
+}
+
+// e.g.
+// source "https://rubygems.org"
+fn parse_source<'i>(i: &mut Input<'i>) -> Res<Item<'i>> {
+    preceded("source ", delimited('"', parse_source_url, '"')).parse_next(i)
+}
+
+fn parse_source_url<'i>(i: &mut Input<'i>) -> Res<Item<'i>> {
+    take_until(0.., '"').map(Item::Source).parse_next(i)
 }
 
 #[cfg(test)]
@@ -33,6 +58,9 @@ mod tests {
 
     #[test]
     fn basic() {
-        todo!()
+        let input = r#"source "https://rubygems.org"
+"#;
+        let out = parse(input).unwrap();
+        assert_eq!(out.items, vec![Item::Source("https://rubygems.org")]);
     }
 }
