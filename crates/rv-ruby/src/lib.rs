c5437bdaf32f01c30faaa5fa7a1ad9d1d6a1c306
David Rodríguez <2887858+deivid-rodriguez@users.noreply.github.com>
2026-01-12 18:09:06 +0100

    Skip new "managed" attribute in tracing

--- crates/rv-ruby/src/lib.rs
+++ crates/rv-ruby/src/lib.rs
@@ -59,7 +59,7 @@ pub struct Ruby {
 
 impl Ruby {
     /// Create a new Ruby instance from a directory path
-    #[instrument(skip(dir), fields(dir = %dir.as_str()))]
+    #[instrument(skip(dir, managed), fields(dir = %dir.as_str()))]
     pub fn from_dir(dir: Utf8PathBuf, managed: bool) -> Result<Self, RubyError> {
         let dir_name = dir.file_name().unwrap_or("");
 
