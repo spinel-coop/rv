9912226e9612c23d77a73145fb505388c0f439b1
David Rodríguez <2887858+deivid-rodriguez@users.noreply.github.com>
2026-01-21 21:20:46 +0100

    Fix `rv tool install` always thinking the tool is installed

---
+++ crates/rv/tests/integration_tests/tool/install_test.rs
@@ -0,0 +1,63 @@
+use crate::common::{RvOutput, RvTest};
+use owo_colors::OwoColorize;
+use rv_cache::rm_rf;
+
+impl RvTest {
+    pub fn tool_install(&mut self, args: &[&str]) -> RvOutput {
+        self.env.remove("RV_INSTALL_URL");
+        self.rv(&[
+            &["tool", "install", "--gem-server", &self.server_url()],
+            args,
+        ]
+        .concat())
+    }
+}
+
+#[test]
+fn test_tool_install_twice() {
+    let mut test = RvTest::new();
+
+    let releases_mock = test.mock_releases(["4.0.0"].to_vec());
+
+    let info_endpoint_content = fs_err::read("tests/fixtures/info-test-gem").unwrap();
+    let info_endpoint_mock = test
+        .mock_info_endpoint("test-gem", &info_endpoint_content)
+        .create();
+
+    let tarball_content =
+        fs_err::read("../rv-gem-package/tests/fixtures/test-gem-1.0.0.gem").unwrap();
+    let tarball_mock = test
+        .mock_gem_download("test-gem-1.0.0.gem", &tarball_content)
+        .create();
+
+    let output = test.tool_install(&["test-gem"]);
+    output.assert_success();
+
+    let tool_home = "/tmp/home/.local/share/rv/tools/test-gem-1.0.0";
+    let expected_info_message = format!(
+        "Installed {} version 1.0.0 to {}",
+        "test-gem".cyan(),
+        tool_home.cyan()
+    );
+
+    let stdout = output.normalized_stdout();
+    assert!(stdout.contains(&expected_info_message), "{}", stdout);
+
+    releases_mock.assert();
+    info_endpoint_mock.assert();
+    tarball_mock.assert();
+
+    // Manually remove tool
+    rm_rf(
+        test.temp_home()
+            .join(".local/share/rv/tools/test-gem-1.0.0"),
+    )
+    .unwrap();
+
+    // Check it succeeds a second time
+    let output = test.tool_install(&["test-gem"]);
+    output.assert_success();
+
+    let stdout = output.normalized_stdout();
+    assert!(stdout.contains(&expected_info_message), "{}", stdout);
+}
