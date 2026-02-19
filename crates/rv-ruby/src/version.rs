9e9a2397fca90e280ab6df689cbd3ea1e6e37178
André Arko <andre@arko.net>
2026-02-19 00:35:54 -0800

    include engine in ruby-dev display string
    this fixes the download URL for dev versions

--- crates/rv-ruby/src/version.rs
+++ crates/rv-ruby/src/version.rs
@@ -40,7 +40,7 @@ impl FromStr for RubyVersion {
 impl std::fmt::Display for RubyVersion {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            RubyVersion::Dev => write!(f, "dev"),
+            RubyVersion::Dev => write!(f, "ruby-dev"),
             RubyVersion::Released(version) => version.fmt(f),
         }
     }
