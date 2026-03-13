c4630ea0fada556b7b3ed85a0694180754f6e0c7
David Rodríguez <2887858+deivid-rodriguez@users.noreply.github.com>
2026-03-13 22:18:31 +0100

    Remove serde derive macros from lockfile types
    So I may be completely off here, but here's my thinking.
    
    Recently we added the ability to deserialize the lockfile type back to
    its original format. So now we can serialize and deserialize the
    propietary Gemfile.lock format into and from our GemfileDotLock type.
    
    Also, all of our tests now check that the roundtrip works ok for all
    Gemfile.lock fixtures and produces no changes from the original file.
    
    So my thinking is that we don't need to involve serde data formats here
    anymore because of the above and because we are all familiar with the
    Gemfile.lock format. It was useful to get the parser tested initially,
    but I don't think that's the case anymore?
    
    The reason I'm proposing this is that the YAML snapshots constantly get
    in the middle when making tweaks & fixes to the parser, and I have to
    either blindly accept the changes through `cargo insta review`, or try
    tweak serde derive macros to keep generating the same thing.

--- crates/rv-lockfile/src/tests.rs
+++ crates/rv-lockfile/src/tests.rs
@@ -1,43 +1,37 @@
 #[test]
 fn test_parse_file() {
     let input = include_str!("../tests/inputs/Gemfile.tapioca.lock");
-    let output = crate::parse(input).unwrap();
-    insta::assert_yaml_snapshot!(output);
+    must_parse(input);
 }
 
 #[test]
 fn test_parse_file_two_sources() {
     let input = include_str!("../tests/inputs/Gemfile.twosources.lock");
-    let output = must_parse(input);
-    insta::assert_yaml_snapshot!(output);
+    must_parse(input);
 }
 
 #[test]
 fn test_parse_empty_sections() {
     let input = include_str!("../tests/inputs/Gemfile.empty.lock");
-    let output = must_parse(input);
-    insta::assert_yaml_snapshot!(output);
+    must_parse(input);
 }
 
 #[test]
 fn test_parse_with_checksums() {
     let input = include_str!("../tests/inputs/Gemfile.withchecksums.lock");
-    let output = must_parse(input);
-    insta::assert_yaml_snapshot!(output);
+    must_parse(input);
 }
 
 #[test]
 fn test_parse_with_paths() {
     let input = include_str!("../tests/inputs/Gemfile.withpath.lock");
-    let output = must_parse(input);
-    insta::assert_yaml_snapshot!(output);
+    must_parse(input);
 }
 
 #[test]
 fn test_parse_with_glob() {
     let input = include_str!("../tests/inputs/Gemfile.withglob.lock");
-    let output = must_parse(input);
-    insta::assert_yaml_snapshot!(output);
+    must_parse(input);
 }
 
 #[test]
@@ -45,41 +39,35 @@ fn test_parse_feedyouremail() {
     let input = include_str!("../tests/inputs/Gemfile.feedyouremail.lock");
     let output = must_parse(input);
     assert_eq!(output.dependencies.len(), 52);
-    insta::assert_yaml_snapshot!(output);
 }
 
 #[test]
 fn test_parse_gitlab() {
     let input = include_str!("../tests/inputs/Gemfile.gitlab.lock");
-    let output = must_parse(input);
-    insta::assert_yaml_snapshot!(output);
+    must_parse(input);
 }
 
 #[test]
 fn test_parse_gemdir() {
     let input = include_str!("../tests/inputs/Gemfile.gemdir.lock");
-    let output = must_parse(input);
-    insta::assert_yaml_snapshot!(output);
+    must_parse(input);
 }
 
 #[test]
 fn test_parse_git_gem() {
     let input = include_str!("../tests/inputs/Gemfile.git.lock");
-    let output = must_parse(input);
-    insta::assert_yaml_snapshot!(output);
+    must_parse(input);
 }
 
 #[test]
 fn test_parse_git_rails() {
     let input = include_str!("../tests/inputs/Gemfile.git-rails.lock");
-    let output = must_parse(input);
-    insta::assert_yaml_snapshot!(output);
+    must_parse(input);
 }
 #[test]
 fn test_parse_discourse() {
     let input = include_str!("../tests/inputs/Gemfile.discourse.lock");
-    let output = must_parse(input);
-    insta::assert_yaml_snapshot!(output);
+    must_parse(input);
 }
 
 #[test]
@@ -88,15 +76,13 @@ fn test_parse_withoutsource() {
     // which provides the endpoints needed to resolve a lockfile successfully, but does not
     // provide the endpoints needed to record checksums. So this lock has empty checksums.
     let input = include_str!("../tests/inputs/Gemfile.withoutsource.lock");
-    let output = must_parse(input);
-    insta::assert_yaml_snapshot!(output);
+    must_parse(input);
 }
 
 #[test]
 fn test_parse_commit_watcher() {
     let input = include_str!("../tests/inputs/Gemfile.commit-watcher.lock");
-    let output = must_parse(input);
-    insta::assert_yaml_snapshot!(output);
+    must_parse(input);
 }
 
 #[test]
@@ -104,8 +90,7 @@ fn test_parse_git_ref() {
     // Test parsing GIT sections with a `ref:` field, like from huginn's Gemfile.lock
     // https://github.com/huginn/huginn/blob/master/Gemfile.lock#L51
     let input = include_str!("../tests/inputs/Gemfile.git-ref.lock");
-    let output = must_parse(input);
-    insta::assert_yaml_snapshot!(output);
+    must_parse(input);
 }
 
 #[test]
@@ -113,8 +98,7 @@ fn test_parse_git_tag() {
     // Test parsing GIT sections with a `tag:` field, like from ekylibre's Gemfile.lock
     // https://github.com/ekylibre/ekylibre/blob/main/Gemfile.lock
     let input = include_str!("../tests/inputs/Gemfile.git-tag.lock");
-    let output = must_parse(input);
-    insta::assert_yaml_snapshot!(output);
+    must_parse(input);
 }
 
 #[test]
@@ -124,7 +108,6 @@ fn test_parse_lobsters() {
     let input = include_str!("../tests/inputs/Gemfile.lobsters.lock");
     let output = must_parse(input);
     assert_eq!(output.ruby_version.clone().unwrap().content(), "ruby 4.0.0");
-    insta::assert_yaml_snapshot!(output);
 }
 
 #[test]
@@ -132,8 +115,7 @@ fn test_parse_mastodon() {
     // Test parsing Mastodon's Gemfile.lock (has `ref:` field in GIT section)
     // https://github.com/mastodon/mastodon
     let input = include_str!("../tests/inputs/Gemfile.mastodon.lock");
-    let output = must_parse(input);
-    insta::assert_yaml_snapshot!(output);
+    must_parse(input);
 }
 
 fn must_parse(input: &str) -> crate::datatypes::GemfileDotLock<'_> {
