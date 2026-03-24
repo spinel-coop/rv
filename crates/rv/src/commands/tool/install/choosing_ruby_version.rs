24034a032a0e1245be56bd4053507192f3469221
David Rodríguez <2887858+deivid-rodriguez@users.noreply.github.com>
2026-03-24 21:31:39 +0100

    Reuse prerelease logic in `Requirement` type

--- crates/rv/src/commands/tool/install/choosing_ruby_version.rs
+++ crates/rv/src/commands/tool/install/choosing_ruby_version.rs
@@ -14,39 +14,26 @@ pub async fn ruby_to_use_for(
 ) -> Result<RubyVersion> {
     let installed_rubies = config.rubies();
 
-    if let Ok(local_ruby) =
-        select_ruby_version_for(&installed_rubies, ruby_constraints, MatchPrereleases::No)
-    {
+    if let Ok(local_ruby) = select_ruby_version_for(&installed_rubies, ruby_constraints, false) {
         return Ok(local_ruby);
     }
 
     let remote_rubies = &config.remote_rubies().await;
-    select_ruby_version_for(remote_rubies, ruby_constraints, MatchPrereleases::No).or_else(|_| {
-        select_ruby_version_for(remote_rubies, ruby_constraints, MatchPrereleases::Yes)
-    })
-}
-
-#[derive(Debug, Eq, PartialEq)]
-enum MatchPrereleases {
-    Yes,
-    No,
+    select_ruby_version_for(remote_rubies, ruby_constraints, false)
+        .or_else(|_| select_ruby_version_for(remote_rubies, ruby_constraints, true))
 }
 
 /// Find the highest Ruby version that meets these constraints from the available choices.
 fn select_ruby_version_for(
     candidate_rubies: &[rv_ruby::Ruby],
     ruby_constraints: &Requirement,
-    match_prereleases: MatchPrereleases,
+    match_prereleases: bool,
 ) -> std::result::Result<RubyVersion, Error> {
     // Otherwise, we'll use the latest Ruby the gem allows.
     for candidate_ruby in candidate_rubies.iter().rev() {
         let version = &candidate_ruby.version;
 
-        if version.is_prerelease() && match_prereleases == MatchPrereleases::No {
-            continue;
-        }
-
-        if ruby_constraints.satisfied_by(&rv_version::Version::from(version)) {
+        if ruby_constraints.matches(&rv_version::Version::from(version), match_prereleases) {
             return Ok(version.clone());
         }
     }
@@ -91,8 +78,7 @@ mod tests {
         let rubies = vec![ruby("ruby-3.2.10"), ruby("ruby-3.3.10"), ruby("ruby-3.4.8")];
 
         let expected = RubyVersion::from_str("ruby-3.3.10").unwrap();
-        let selected_ruby =
-            select_ruby_version_for(&rubies, &requirement, MatchPrereleases::No).unwrap();
+        let selected_ruby = select_ruby_version_for(&rubies, &requirement, false).unwrap();
 
         assert_eq!(expected, selected_ruby);
     }
@@ -113,13 +99,13 @@ mod tests {
         ];
 
         let expected = RubyVersion::from_str("ruby-3.5.0-preview1").unwrap();
-        let match_prereleases = MatchPrereleases::Yes;
+        let match_prereleases = true;
         let selected_ruby =
             select_ruby_version_for(&rubies, &requirement, match_prereleases).unwrap();
         assert_eq!(expected, selected_ruby);
 
         let expected = RubyVersion::from_str("ruby-3.4.8").unwrap();
-        let match_prereleases = MatchPrereleases::No;
+        let match_prereleases = false;
         let selected_ruby =
             select_ruby_version_for(&rubies, &requirement, match_prereleases).unwrap();
         assert_eq!(expected, selected_ruby);
